72e184ae44ca84fed79e3d19bb70a2070478158b
Nickolay Kulikov <nkulikov@bssl.ru>
2022-02-20 16:37:10 +0300

    updated server

--- server/index.js
+++ server/index.js
@@ -8,9 +8,17 @@ const SERVER_PORT = 3000;
 
 const server = http.createServer((req, res) => {
   const { url } = req;
+
   log('request', req.url);
 
-  const fileName = url === '/' ? '/index.html' : url;
+  const regex = '[^\n]+[.][^\n]+';
+  const nestedFileName = `${url}${url}`.match(regex) ? `${url.split('.')[0]}${url}`.match(regex)[0] : `${url}${url}.html`;
+  const isIndex = `${url}`.match('[^\n]+[index][^\n]+');
+  const indexFileName = isIndex ? `${url}`.match('[^\n]+[index][^\n]+')[0] : '/index.html';
+
+  const fileName = url === '/' || isIndex ? indexFileName : nestedFileName;
+
+  log('filename:', fileName);
 
   fs.readFile(`${__dirname}/../public/${fileName}`, (err, file) => {
     if (err) {

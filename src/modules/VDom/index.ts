538a5d0d29d1002f15c12db7da4acc011ff28212
rflban <nabiev.alsn@yandex.ru>
2022-03-27 20:11:18 +0300

    WV-55: update VDom index

--- src/modules/VDom/index.ts
+++ src/modules/VDom/index.ts
@@ -1,7 +1,13 @@
-import { default as render } from './render';
-import { default as createElement } from './createElement';
+import render from './render';
+import createElement from './createElement';
+import Component from './Component';
+import Ref from './Ref';
+import VirtualElement from './VirtualElement';
 
 export default {
   render,
   createElement,
+  Component,
+  Ref,
+  VirtualElement,
 };

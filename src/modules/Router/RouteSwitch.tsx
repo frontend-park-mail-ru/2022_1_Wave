d46569bec1efeb15d2a43173ebe5ac714622acd2
rflban <nabiev.alsn@yandex.ru>
2022-04-06 13:59:03 +0300

    WV-55: add router match function

--- src/modules/Router/RouteSwitch.tsx
+++ src/modules/Router/RouteSwitch.tsx
@@ -1,8 +1,9 @@
 import VDom from '../VDom';
 import Route from './Route';
-import { ContextType, IContextType } from '../VDom/Context';
+import { Context, ContextType, IContext, IContextType } from '../VDom/Context';
 
-const pathRegExp = /^\/?(?::?[\w_~.-])+(?\/)$/;
+const routeRegExp = /^(?:\/:?[\w_~.-]+)*$/;
+const pathRegExp = /^(?:\/[\w_~.-]+)*$/;
 
 type RouteProps = {
   to: string,
@@ -12,35 +13,68 @@ type RouteProps = {
 class Router {
   #workingPath: string;
 
+  handledPaths: string[];
+
+  unhandledPath: string;
+
   get workingPath(): string {
     return this.#workingPath;
   }
 
   constructor() {
     this.#workingPath = '';
+    this.handledPaths = [];
+    this.unhandledPath = window.location.pathname;
   }
 }
 
 const routerContextType = new ContextType<Router | null>('router', null);
 
-function match(expectPath: string, actualPath: string, isExact: bool): bool {
+function match(expectPath: string, actualPath: string, isExact: boolean):
+  { params: any, rest: string } | null {
+  if (!expectPath.match(routeRegExp)) {
+    throw Error(`Invalid route: ${expectPath}`);
+  }
+  if (!actualPath.match(pathRegExp)) {
+    throw Error(`Invalid path: ${actualPath}`);
+  }
+
   const expect: string[] = expectPath.split('/');
   const actual: string[] = actualPath.split('/');
+  const rest = `/${actual.slice(expect.length).join('/')}`;
+  const params: any = {};
 
   if (expect.length > actual.length) {
-    return false;
+    return null;
   }
 
   if (isExact && expect.length !== actual.length) {
-    return false;
+    return null;
   }
 
-  expect.forEach((expectFrag, idx) => {
+  const isMatching = expect.every((expectFrag, idx) => {
     const actualFrag = actual[idx];
+
+    if (expectFrag.startsWith(':')) {
+      params[expectFrag.slice(1)] = actualFrag;
+      return true;
+    }
+
+    return expectFrag === actualFrag;
   });
 
-  return true;
+  if (!isMatching) {
+    return null;
+  }
+
+  return { params, rest };
 }
+
+// class Parent extends VDom.Component {
+//   produceContext(): IContext {
+//     return new Context(routerContextType, new Router());
+//   }
+// }
 
 export default class RouteSwitch extends VDom.Component<any, any, any, Router> {
   get contextType(): IContextType {
@@ -53,6 +87,13 @@ export default class RouteSwitch extends VDom.Component<any, any, any, Router> {
     for (const child of this.children) {
       if (child instanceof VDom.VirtualElement && child.type === Route) {
         const { to: path, exact: isExact } = child.props as RouteProps;
+
+        const matchResult = match(path, router.unhandledPath, isExact);
+
+        if (matchResult) {
+          const { params, rest } = matchResult;
+          router.unhandledPath = rest;
+        }
       } else {
         throw Error('RouteSwitch have to contain only Route components');
       }

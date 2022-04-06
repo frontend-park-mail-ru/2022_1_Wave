d297c6afb1e55bf8a5de735b0423b117b8651ea1
rflban <nabiev.alsn@yandex.ru>
2022-04-06 14:10:52 +0300

    WV-55: update RouteSwitch component

--- src/modules/Router/RouteSwitch.tsx
+++ src/modules/Router/RouteSwitch.tsx
@@ -1,6 +1,6 @@
 import VDom from '../VDom';
 import Route from './Route';
-import { Context, ContextType, IContext, IContextType } from '../VDom/Context';
+import { ContextType, IContextType } from '../VDom/Context';
 
 const routeRegExp = /^(?:\/:?[\w_~.-]+)*$/;
 const pathRegExp = /^(?:\/[\w_~.-]+)*$/;
@@ -84,22 +84,23 @@ export default class RouteSwitch extends VDom.Component<any, any, any, Router> {
   render(): VDom.VirtualElement {
     const router = this.ctx.value;
 
-    for (const child of this.children) {
-      if (child instanceof VDom.VirtualElement && child.type === Route) {
-        const { to: path, exact: isExact } = child.props as RouteProps;
+    if (!this.children.every((child) => child instanceof VDom.VirtualElement)) {
+      throw Error('RouteSwitch have to contain only Route components');
+    }
 
-        const matchResult = match(path, router.unhandledPath, isExact);
+    for (const childNode of this.children) {
+      const child = childNode as VDom.VirtualElement;
+      const { to: path, exact: isExact } = child.props as RouteProps;
 
-        if (matchResult) {
-          const { params, rest } = matchResult;
-          router.unhandledPath = rest;
+      const matchResult = match(path, router.unhandledPath, isExact);
 
-          child.props.params = params;
+      if (matchResult) {
+        const { params, rest } = matchResult;
+        router.unhandledPath = rest;
 
-          return child;
-        }
-      } else {
-        throw Error('RouteSwitch have to contain only Route components');
+        child.props.params = params;
+
+        return child;
       }
     }
 

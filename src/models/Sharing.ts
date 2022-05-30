c7b1494bf08f6973c93d463f385c9d9f5012193f
Faris Nabiev <f.nabiev@vk.team>
2022-05-30 12:14:48 +0300

    WV-61: update sharing

--- src/models/Sharing.ts
+++ src/models/Sharing.ts
@@ -3,14 +3,20 @@ import Paths from '../config/Sharing';
 
 export default class Sharing {
   static shrink(url: string): Promise<any> {
-    return HTTPClient.post(Paths.schema, { url }).then((response) => {
-      if (response.status !== 200) {
-        return Promise.reject(response.body);
-      }
-      return response.body.Result.hash;
-    }).catch((e) => {
-      console.error('Sharing.shrink', e);
-      return url;
+    return fetch(Paths.schema, {
+      method: 'POST',
+      body: JSON.stringify({ url }),
     })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.status !== 200) {
+          return Promise.reject(response.body);
+        }
+        return response.body.Result.hash;
+      })
+      .catch((e) => {
+        console.error('Sharing.shrink', e);
+        return url;
+      })
   }
 }
